ff0503aeb9339b0d5eddb671b8cb3d5e09dfe469
Jeffrey Warren <jtwarren@mit.edu>
2014-04-22 18:22:09 -0400

    initial push of android application with scan service

---
+++ src/com/example/spidey/ScanService.java
@@ -0,0 +1,44 @@
+package com.example.spidey;
+
+import java.util.List;
+
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.os.IBinder;
+import android.telephony.CellIdentityGsm;
+import android.telephony.CellInfo;
+import android.telephony.CellInfoGsm;
+import android.telephony.CellSignalStrengthGsm;
+import android.telephony.TelephonyManager;
+import android.util.Log;
+
+public class ScanService extends Service {
+	
+	private TelephonyManager telephonyManager;
+
+	@Override
+	public int onStartCommand(Intent intent, int flags, int startId) {
+		this.telephonyManager = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
+
+		List<CellInfo> cellInfos = (List<CellInfo>) this.telephonyManager.getAllCellInfo();
+
+		for (CellInfo cellInfo : cellInfos) {
+			CellInfoGsm cellInfoGsm = (CellInfoGsm) cellInfo;
+
+			CellIdentityGsm cellIdentity = cellInfoGsm.getCellIdentity();
+			CellSignalStrengthGsm cellSignalStrengthGsm = cellInfoGsm.getCellSignalStrength();
+
+			Log.d("cell", "registered: " + cellInfoGsm.isRegistered());
+			Log.d("cell", cellIdentity.toString());
+			Log.d("cell", cellSignalStrengthGsm.toString());
+		}
+		return Service.START_NOT_STICKY;
+	}
+
+	@Override
+	public IBinder onBind(Intent intent) {
+		// TODO for communication return IBinder implementation
+		return null;
+	}
+}
