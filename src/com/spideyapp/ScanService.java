b3f26ea0f4063181b2a110bfe0bfd58da63e2a95
n8fr8 <nathan@freitas.net>
2014-05-13 00:20:04 -0400

    add cellinfo result broadcast service->map activity

--- src/com/spideyapp/ScanService.java
+++ src/com/spideyapp/ScanService.java
@@ -41,6 +41,9 @@ public class ScanService extends Service {
 		lastScanLat = intent.getDoubleExtra("lat", 0.0);
 		lastScanLon = intent.getDoubleExtra("lon", 0.0);
 		
+		if (DEBUG == true)
+			startDummyScan();
+		
 		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1)
 			startScan();
 		else
@@ -88,6 +91,8 @@ public class ScanService extends Service {
 
 					db.createCell(cell, scan_id);
 
+					shareCellInfo (cell);
+					
 					logMessage("registered: " + cellInfoGsm.isRegistered()
 							+ ",cellId: " + cellIdentity.toString()
 							+ ",cellSignalStrength: "
@@ -97,6 +102,39 @@ public class ScanService extends Service {
 		}
 
 	}
+	
+	@TargetApi(Build.VERSION_CODES.JELLY_BEAN_MR1)
+	private void startDummyScan() {
+		db = DatabaseHelper.getInstance(getApplicationContext());
+		
+		logMessage("starting tower scan... ");
+		Scan scan = new Scan();
+
+		// TODO: Get location from user?
+		scan.setLocation(lastScanName);
+
+		// TODO: use actual GPS Coordinates
+		scan.setLatitude(lastScanLat);
+		scan.setLongitude(lastScanLon);
+
+		long scan_id = db.createScan(scan);
+
+		for (int i = 0; i < 5; i++)
+		{
+			com.spideyapp.sqlite.model.CellInfo cell = new com.spideyapp.sqlite.model.CellInfo(
+					i, 100,
+					200, 300, 10*i);
+
+			db.createCell(cell, scan_id);
+
+			shareCellInfo (cell);
+					
+					
+			
+		}
+
+	}
+
 
 	private void startSimpleScan() {
 		logMessage("starting tower scan (old stylie)... ");
@@ -138,4 +176,15 @@ public class ScanService extends Service {
 		// TODO for communication return IBinder implementation
 		return null;
 	}
+	
+	public static final String NOTIFICATION = "com.spideyapp.service.receiver";
+	
+	 private void shareCellInfo(com.spideyapp.sqlite.model.CellInfo cellInfo) {
+		    Intent intent = new Intent(NOTIFICATION);
+		    intent.putExtra("cid",cellInfo.getCID());
+		    intent.putExtra("dbm",cellInfo.getDBM());
+		    
+		  
+		    sendBroadcast(intent);
+		  }
 }
